fcf3365d2f492059a4e7cb26b547d802d3fa33a5
feederbox826 <feederbox826@users.noreply.github.com>
2024-07-08 18:32:15 -0400

    add real gist support

--- worker.js
+++ worker.js
@@ -2,20 +2,30 @@ export default {
   async fetch(request, env, ctx) {
     const reqUrl = new URL(request.url);
     const reponame = reqUrl.pathname;
+    const headers = {
+      "User-Agent": "fbox826/last-commit",
+    };
     let gist = Boolean(reqUrl.searchParams.get("gist"));
     // repo regex
     if (!/^\/[\w\.-]+\/[\w\.-]+$/.test(reponame)) return new Response("null");
     // gist regex
-    if (/^\/[\w\.-]+\/[a-f0-9]{32}$/.test(reponame)) gist = true;
-    const type = gist ? "gist" : "repos";
-    const apiUrl = `https://api.github.com/${type}${reponame}/commits`;
-    const date = await fetch(apiUrl, {
-      headers: {
-        "User-Agent": "fbox826/last-commit",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => data[0].commit.author.date.split("T")[0]);
-    return new Response(`<date>${date}</date>`);
+    if (/^\/[a-f0-9]{32}$/.test(reponame)) gist = true;
+    if (gist) {
+      const apiRepoURL = `https://api.github.com/gists${reponame}`;
+      const date = await fetch(apiRepoURL, {
+        headers,
+      })
+        .then((response) => response.json())
+        .then((data) => data.updated_at.split("T")[0]);
+      return new Response(`<date>${date}</date>`);
+    } else {
+      const apiRepoURL = `https://api.github.com/repos${reponame}/commits`;
+      const date = await fetch(apiRepoURL, {
+        headers,
+      })
+        .then((response) => response.json())
+        .then((data) => data[0].commit.author.date.split("T")[0]);
+      return new Response(`<date>${date}</date>`);
+    }
   },
 };
