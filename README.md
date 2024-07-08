e5132f13075945c8e56fedae3c2f28b4c9a802d3
feederbox826 <feederbox826@users.noreply.github.com>
2024-07-08 18:10:51 -0400

    move to GH

---
+++ README.md
@@ -0,0 +1,2 @@
+# last-commit
+Gets last commit of a gist/ repo from github and exports it in XML format for GSheets
---
+++ worker.js
@@ -0,0 +1,21 @@
+export default {
+  async fetch(request, env, ctx) {
+    const reqUrl = new URL(request.url);
+    const reponame = reqUrl.pathname;
+    const gist = reqUrl.searchParams.get("gist");
+    // repo regex
+    if (!/^\/[\w\.-]+\/[\w\.-]+$/.test(reponame)) return new Response("null");
+    // gist regex
+    if (/^\/[\w\.-]+\/[a-f0-9]{32}$/.test(reponame)) gist = true;
+    const type = gist ? "gist" : "repos";
+    const apiUrl = `https://api.github.com/${type}${reponame}/commits`;
+    const date = await fetch(apiUrl, {
+      headers: {
+        "User-Agent": "fbox826/last-commit",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => data[0].commit.author.date.split("T")[0]);
+    return new Response(`<date>${date}</date>`);
+  },
+};
