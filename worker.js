80f97c61198da3708b543eb8cf29a0a552e6ce38
feederbox826 <feederbox826@users.noreply.github.com>
2024-08-21 20:48:27 -0400

    minor optimizations
    - use ternary operator instead of if
    - don't needlessly update TTL
    - split await

--- worker.js
+++ worker.js
@@ -1,81 +1,79 @@
 const headers = {
-  "User-Agent": "fbox826/last-commit/v1",
-};
-const returnDate = (field) => `<date>${field.split("T")[0]}</date>`;
-const repoRegex = new RegExp(/^\/[\w\.-]+\/[\w\.-]+$/);
-const gistRegex = new RegExp(/^\/[a-f0-9]{32}$/);
+  "User-Agent": "fbox826/last-commit/v1"
+}
+const returnDate = (field) => `<date>${field.split("T")[0]}</date>`
+const repoRegex = new RegExp(/^\/[\w\.-]+\/[\w\.-]+$/)
+const gistRegex = new RegExp(/^\/[a-f0-9]{32}$/)
 
-const gistLookup = async (reponame) =>
-  fetch(
-    `https://api.github.com/gists${reponame}`,
-    { headers},
-  )
-    .then((response) => response.json())
-    .then((data) => data.updated_at)
-    .catch((err) => err);
+const gistLookup = (reponame) =>
+  fetch(`https://api.github.com/gists${reponame}`, { headers })
+    .then(response => response.json())
+    .then(data => data.updated_at)
+    .catch(err => err)
 
-const repoLookup = async (reponame) =>
-  fetch(
-    `https://api.github.com/repos${reponame}/commits`,
-    { headers },
-  )
-    .then((response) => response.json())
-    .then((data) => data[0].commit.author.date)
-    .catch((err) => err);
+const repoLookup = (reponame) =>
+  fetch(`https://api.github.com/repos${reponame}/commits`, { headers })
+    .then(response => response.json())
+    .then(data => data[0].commit.author.date)
+    .catch(err => err)
 
 // tiered caching
 const cacheTtl = (date) => {
-  const diff = Date.now() - Date.parse(date);
-  if (isNaN(diff)) return 0;
+  const parsedDate = Date.parse(date)
+  if (isNaN(parsedDate)) return 0
+  const diff = Date.now() - parsedDate
+  if (isNaN(diff)) return 0
   const day = 86400
   const week = 604800
   const month = 2592000
-  // if less than 1w, cache 1d
-  if (diff < week * 1000) {
-    return day;
-  } else if (diff < month * 1000) {
-    // if less than 1m, cache 1w
-    return week;
-  } else if (diff > month * 1000) {
-    // if more than 1m, cache 1m
-    return month;
-  }
-  return 0;
+  return diff < week * 1000
+    ? day // if less than 1wk, cache 1d
+    : diff < month * 1000
+      ? week // if less than 1mo, cache 1wk
+      : diff > month * 1000
+        ? month // if more than 1mo, cache 1mo
+        : 0
 }
 
-const cachePut = async (reponame, date, env) => {
-  const expirationTtl = cacheTtl(date);
-  if (expirationTtl === 0) return
+const cachePut = (reponame, date, env) => {
+  const expirationTtl = cacheTtl(date)
   // only set if value is different
-  const currentValue = await env.KV_COMMITS.get(reponame);
-  if (currentValue === date) return
-  await env.KV_COMMITS.put(reponame, date, { expirationTtl });
+  if (expirationTtl === 0) return
+  env.KV_COMMITS.put(reponame, date, { expirationTtl })
+}
+
+const ghLookup = async (reponame, env) => {
+  // look up and cache
+  const lookup = gistRegex.test(reponame)
+    ? await gistLookup(reponame)
+    : repoRegex.test(reponame)
+      ? await repoLookup(reponame)
+      : "null"
+  // async cache put
+  cachePut(reponame, lookup, env)
+  return lookup
 }
 
 // KV lookup
-const kvLookup = async (reponame, env, skip = false) => {
-  // tiered cache lookup
-  const cached = await env.KV_COMMITS.get(reponame);
-  let res = cached
-    ? cached || skip
-    : gistRegex.test(reponame)
-      ? gistLookup(reponame)
-      : repoRegex.test(reponame)
-        ? repoLookup(reponame)
-        : "null";
-  const resolv = await res
-  if (resolv == "null" || isNaN(Date.parse(resolv))) return "null";
-  await cachePut(reponame, resolv, env);
-  return returnDate(resolv);
+const splitLookup = async (reponame, env, skip = false) => {
+  // prefer KV lookup, fallback
+  const kvResult = await env.KV_COMMITS.get(reponame, { cacheTtl: 86400 })
+  if (kvResult) return returnDate(kvResult)
+  // ghLookup as fallback
+  const ghResult = await ghLookup(reponame, env)
+  if (ghResult == "null" || isNaN(Date.parse(ghResult))) return "null"
+  return returnDate(ghResult)
 }
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     const reponame = url.pathname;
-    const skip = url.searchParams.get("refresh") === "true";
+    const skip = url.searchParams.get("refresh") === "true"
     // look up in KV
-    const res = await kvLookup(reponame, env, skip,);
-    return new Response(res);
+    const res = skip
+      ? await ghLookup(reponame, env)
+      : await splitLookup(reponame, env)
+    return new Response(res)
   }
-};
+}
